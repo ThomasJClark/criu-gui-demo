3b063f0afd03fd4f3a4b5767030da7ce694f0e42
Tom Clark <tclark@redhat.com>
2015-07-21 16:24:38 -0400

    Add documentation for /procs

--- procs.py
+++ procs.py
@@ -3,7 +3,17 @@ import json
 
 
 class Procs:
+    """
+        This class is responsible for listing the processes on this machine as
+        a JSON object, where children processes are stored hierarchically
+        beneath their parent processes.
+    """
+
     def GET(self):
+        """
+            Respond to an HTTP GET request with a JSON string.
+        """
+
         pidstats = procfs.pidstats()
         flatprocs = []
         root = {}
@@ -29,6 +39,11 @@ class Procs:
         return json.dumps(root, separators=",:")
 
     def unflatten(self, flatprocs, proc):
+        """
+            Utility to convert a flat list of processes with reference to their
+            parents' PIDs into a tree.
+        """
+
         del proc["parent"]
         for proc2 in flatprocs:
             if proc2["parent"] == proc["id"]:
