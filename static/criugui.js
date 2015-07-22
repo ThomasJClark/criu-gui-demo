0ad723661e0a1f3d422902cc63953fa8f336b69b
Tom Clark <tclark@redhat.com>
2015-07-22 11:20:46 -0400

    Made the tree horizontal
    This should free up some space for labels

--- static/criugui.js
+++ static/criugui.js
@@ -5,38 +5,39 @@ d3.select("svg")
 var nameLabel = d3.select("#process-name");
 var idLabel = d3.select("#process-id");
 var childrenLabel = d3.select("#process-children");
-var g = d3.select("svg").append("g").attr("width", 1000).attr("height", 500);
+var g = d3.select("svg").append("g").attr(
+    {"width" : 800, "height" : 500, "transform" : "translate(25, 0)"});
 
-var diagonal = d3.svg.diagonal();
+var diagonal =
+    d3.svg.diagonal().projection(function(d) { return [ d.y, d.x ]; });
 
 var tree = d3.layout.tree()
-               .size([ g.attr("width"), g.attr("height") ])
+               .size([ g.attr("height"), g.attr("width") ])
                .children(function(d) { return d.children; })
                .value(function(d) { return d.name; });
 
 function redraw(data) {
-  console.log("redrawing...");
-
   var nodeData = tree.nodes(data);
   var linkData = tree.links(nodeData);
 
   var links = g.selectAll("path.link").data(linkData);
-  var nodes = g.selectAll("circle.node").data(nodeData);
+  var nodes = g.selectAll("g.node").data(nodeData);
 
   links.attr("d", diagonal);
   links.enter().append("path").attr("class", "link").attr("d", diagonal);
   links.exit().remove();
 
-  nodes.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
+  nodes.attr("transform",
+             function(d) { return "translate(" + d.y + "," + d.x + ")"; });
 
   nodes.enter()
-      .append("circle")
+      .append("g")
       .attr("class", "node")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-      .attr("r", 8.0)
+      .attr("transform",
+            function(d) { return "translate(" + d.y + "," + d.x + ")"; })
       .on("mouseover", function(d) {
+        d3.select(this).classed("active-node", true);
+
         nameLabel.text(d.name);
         idLabel.text(d.id);
         if (d.children) {
@@ -47,11 +48,15 @@ function redraw(data) {
         }
       })
       .on("mouseout", function(d) {
+        d3.select(this).classed("active-node", false);
+
         nameLabel.text("");
         idLabel.text("");
-        childrenLabel.text(
-            "Hover over a node to see more information about that process.");
-      });
+        childrenLabel.text("Hover over a node to see more information " +
+                           "about that process.");
+      })
+      .append("circle")
+      .attr("r", 4.0);
 
   nodes.exit().remove();
 }
