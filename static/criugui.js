45c0bad4639860f336997f6bba19ebcbfb336220
Tom Clark <tclark@redhat.com>
2015-08-03 16:56:57 -0400

    Clarify the hardcoded addresses and add a readme

--- static/criugui.js
+++ static/criugui.js
@@ -1,8 +1,8 @@
 /* Show two trees.
  * TODO: don't hardcode this. */
 var targetData = [
-    { name: "localhost", address: "" },
-    { name: "beaker machine", address: "http://hp-dl360pgen8-08.khw.lab.eng.bos.redhat.com:8080" },
+    { name: "localhost", address: "http://127.0.0.1:8080" },
+    { name: "other machine", address: "http://some-other-machine-also-running-criugui.py:8080" },
 ];
 
 var pstrees = d3.select("#pstree-container").selectAll("div").data(targetData);
